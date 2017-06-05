ea98cecc9a3c11302979b3fcae0ab296fd05069c
Rowan <rdausilio@gmail.com>
2017-06-05 18:16:25 -0400

    Learning Python
    Exercises from "Learning Python the Hard Way"

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# This line says that I will count the chickens
+print("I will now count my chickens:")
+# This line prints how many Hens I have
+print("Hens", 25 + 30 / 6)
+# This line prints how many roosters I have
+print("Roosters", 100 - 25 * 3 % 4)
+# This line says I will count the eggs
+print("Now I will count the eggs:")
+# This line counts the eggs
+print(3+2+1-5+4 % 2-1/4+6)
+# This line asks a question
+print("Is it true that 3+2 < 5-7?")
+# This line answers the question
+print(3+2 < 5-7)
+# This line  and the one after verify that it is false
+print("What is 3 + 2?", 3+2)
+print("What is 5 - 7?", 5-7)
+# This line is a statement
+print("Oh, that's why its False.")
+# This line says that I am about to ask more math questions
+print("How about some more.")
+# This line asks the math questions and the answers
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5>+ -2)
+print("Is it less or equal?", 5 <= -2)
+
+# Below is the study drill 3. Something that I need ot calculate
+print(3+2)
